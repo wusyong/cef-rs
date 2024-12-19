d9894889bd9845d6c11344347b56aebf5bb6dce7
Bill Avery <billav@microsoft.com>
2024-12-18 17:55:27 -0800

    feat: parse structs into fields and methods

--- update-bindings/src/parse_tree.rs
+++ update-bindings/src/parse_tree.rs
@@ -25,49 +25,221 @@ pub enum Unrecognized {
     Parse(#[from] syn::Error),
 }
 
-enum FieldType<'a> {
-    Value { ty: String },
-    Fn { sig: &'a syn::Signature },
+#[derive(Debug)]
+struct MethodArgument {
+    name: String,
+    ty: String,
 }
 
-impl Debug for FieldType<'_> {
+#[derive(Debug)]
+struct MethodDeclaration {
+    name: String,
+    args: Vec<MethodArgument>,
+    output: Option<String>,
+}
+
+impl Display for MethodDeclaration {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            FieldType::Value { ty } => write!(f, "{}", ty),
-            FieldType::Fn { sig } => write!(f, "fn (...)"),
+        let name = &self.name;
+        let args = self
+            .args
+            .iter()
+            .map(|arg| format!("{}: {}", arg.name, arg.ty))
+            .collect::<Vec<_>>()
+            .join(", ");
+        let output = self
+            .output
+            .as_deref()
+            .map(|output| format!(" -> {output}"))
+            .unwrap_or_default();
+        writeln!(f, "    fn {name}({args}){output} {{}}")
+    }
+}
+
+impl TryFrom<&syn::Field> for MethodDeclaration {
+    type Error = Unrecognized;
+
+    fn try_from(value: &syn::Field) -> Result<Self, Self::Error> {
+        let name = value
+            .ident
+            .as_ref()
+            .ok_or(Unrecognized::FieldType)?
+            .to_string();
+
+        // Look for a type matching std::option::Option<T>
+        let syn::Type::Path(syn::TypePath {
+            qself: None,
+            path: syn::Path { segments, .. },
+        }) = &value.ty
+        else {
+            return Err(Unrecognized::FieldType);
+        };
+        let mut segments_iter = segments.iter();
+        let (
+            Some(syn::PathSegment {
+                ident: ident_std,
+                arguments: syn::PathArguments::None,
+            }),
+            Some(syn::PathSegment {
+                ident: ident_option,
+                arguments: syn::PathArguments::None,
+            }),
+            Some(syn::PathSegment {
+                ident: ident_type,
+                arguments:
+                    syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+                        args, ..
+                    }),
+            }),
+            None,
+        ) = (
+            segments_iter.next(),
+            segments_iter.next(),
+            segments_iter.next(),
+            segments_iter.next(),
+        )
+        else {
+            return Err(Unrecognized::FieldType);
+        };
+        if ident_std.to_string() != "std"
+            || ident_option.to_string() != "option"
+            || ident_type.to_string() != "Option"
+            || args.len() != 1
+        {
+            return Err(Unrecognized::FieldType);
         }
+
+        // See if the Option<T> type is a function pointer
+        let mut args = args.iter();
+        let (
+            Some(syn::GenericArgument::Type(syn::Type::BareFn(syn::TypeBareFn {
+                unsafety: Some(_),
+                abi: Some(syn::Abi {
+                    name: Some(abi), ..
+                }),
+                inputs,
+                variadic: None,
+                output,
+                ..
+            }))),
+            None,
+        ) = (args.next(), args.next())
+        else {
+            return Err(Unrecognized::FieldType);
+        };
+        if abi.value() != "C" {
+            return Err(Unrecognized::FieldType);
+        }
+
+        // Looks like a match, convert it to a MethodDeclaration
+        let args = inputs
+            .iter()
+            .filter_map(|arg| {
+                if let syn::BareFnArg {
+                    name: Some((name, _)),
+                    ty,
+                    ..
+                } = arg
+                {
+                    Some(MethodArgument {
+                        name: name.to_string(),
+                        ty: ty.to_token_stream().to_string(),
+                    })
+                } else {
+                    None
+                }
+            })
+            .collect();
+        let output = match output {
+            syn::ReturnType::Type(_, ty) => Some(ty.to_token_stream().to_string()),
+            _ => None,
+        };
+
+        Ok(Self { name, args, output })
     }
 }
 
 #[derive(Debug)]
-struct FieldDeclaration<'a> {
+struct FieldDeclaration {
     name: String,
-    ty: FieldType<'a>,
+    ty: String,
+}
+
+impl Display for FieldDeclaration {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let name = &self.name;
+        let ty = &self.ty;
+        write!(f, "    {name}: {ty},")
+    }
+}
+
+impl TryFrom<&syn::Field> for FieldDeclaration {
+    type Error = Unrecognized;
+
+    fn try_from(value: &syn::Field) -> Result<Self, Self::Error> {
+        let name = value
+            .ident
+            .as_ref()
+            .ok_or(Unrecognized::FieldType)?
+            .to_string();
+        let ty = value.ty.to_token_stream().to_string();
+
+        Ok(Self { name, ty })
+    }
 }
 
 #[derive(Debug, Default)]
-struct StructDeclaration<'a> {
+struct StructDeclaration {
     name: String,
-    fields: Vec<FieldDeclaration<'a>>,
+    fields: Vec<FieldDeclaration>,
+    methods: Vec<MethodDeclaration>,
 }
 
 #[derive(Debug, Default)]
-struct ParseTree<'a> {
+struct ParseTree {
     type_aliases: BTreeMap<String, String>,
-    structs: Vec<StructDeclaration<'a>>,
+    enums: Vec<String>,
+    structs: Vec<StructDeclaration>,
 }
 
-impl Display for ParseTree<'_> {
+impl Display for ParseTree {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(f, "//! ParseTree")?;
         for (alias_name, alias_ty) in &self.type_aliases {
             writeln!(f, "type {} = {};", alias_name, alias_ty)?;
         }
+        for StructDeclaration {
+            name,
+            fields,
+            methods,
+        } in &self.structs
+        {
+            if fields.is_empty() {
+                writeln!(f, "struct {name};")?;
+            } else {
+                writeln!(f, "struct {name} {{")?;
+                for field in fields {
+                    writeln!(f, "{field}")?;
+                }
+                writeln!(f, "}}")?;
+            }
+
+            if !methods.is_empty() {
+                writeln!(f, "impl {name} {{")?;
+                for method in methods {
+                    writeln!(f, "{method}")?;
+                }
+                writeln!(f, "}}")?;
+            }
+        }
+        for enum_name in &self.enums {
+            writeln!(f, "enum {enum_name};")?;
+        }
         Ok(())
     }
 }
 
-impl<'a> TryFrom<&'a syn::File> for ParseTree<'a> {
+impl TryFrom<&syn::File> for ParseTree {
     type Error = Unrecognized;
 
     fn try_from(value: &syn::File) -> Result<Self, Self::Error> {
@@ -79,26 +251,23 @@ impl<'a> TryFrom<&'a syn::File> for ParseTree<'a> {
                     let alias_ty = item_type.ty.to_token_stream().to_string();
                     tree.type_aliases.insert(alias_name, alias_ty);
                 }
-                // syn::Item::Struct(item_struct) => {
-                //     let mut struct_decl = StructDeclaration::default();
-                //     struct_decl.name = item_struct.ident.to_string();
-                //     for field in &item_struct.fields {
-                //         let field_name = field.ident.as_ref().unwrap().to_string();
-                //         let field_ty = match &field.ty {
-                //             syn::Type::Path(path) => {
-                //                 let ty = path.path.segments.last().unwrap().ident.to_string();
-                //                 FieldType::Value { ty }
-                //             }
-                //             syn::Type::BareFn(bare_fn) => FieldType::Fn { sig: &bare_fn.sig },
-                //             _ => return Err(Unrecognized::FieldType),
-                //         };
-                //         struct_decl.fields.push(FieldDeclaration {
-                //             name: field_name,
-                //             ty: field_ty,
-                //         });
-                //     }
-                //     tree.structs.push(struct_decl);
-                // }
+                syn::Item::Struct(item_struct) => {
+                    if let syn::Fields::Named(fields) = &item_struct.fields {
+                        let mut struct_decl = StructDeclaration::default();
+                        struct_decl.name = item_struct.ident.to_string();
+                        for field in fields.named.iter() {
+                            if let Ok(field_decl) = MethodDeclaration::try_from(field) {
+                                struct_decl.methods.push(field_decl);
+                            } else if let Ok(field_decl) = FieldDeclaration::try_from(field) {
+                                struct_decl.fields.push(field_decl);
+                            }
+                        }
+                        tree.structs.push(struct_decl);
+                    }
+                }
+                syn::Item::Enum(syn::ItemEnum { ident, .. }) => {
+                    tree.enums.push(ident.to_string());
+                }
                 _ => {}
             }
         }
